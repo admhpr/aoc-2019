d284db151959056abef06f3248d2eb901ccf58bb
adam <adam@harpur.io>
2019-12-06 15:11:45 +0000

    feat: part one complete

--- 04/node/index.js
+++ 04/node/index.js
@@ -5,20 +5,18 @@ const inputPath = path.join(__dirname, 'input');
 async function execute(){
     const [input] = fs.readFileSync(inputPath, 'utf8').split("\n");
     const range = input.split("-").map(v => Number(v));
-    part1(range)
+    console.log(part1(range))
 }
 
 function part1([start, end]){
     let current = start;
     const potentialPasswords = [];
-
     while(current < end){
-        if(isSixDigits(current) && hasAdjacent(current) && doesNotDecrease(current)){
+        if(isSixDigits(current) && doesNotDecrease(current) && hasAdjacent(current)){
             potentialPasswords.push(current)
         }
         current += 1;
     }
-
     return potentialPasswords.length
 }
 function isSixDigits(n){
@@ -26,10 +24,12 @@ function isSixDigits(n){
 }
 
 function hasAdjacent(n){
-    return true
+    return new Set(`${n}`.split("").map(v => Number(v))).size !== 6
 }
 
 function doesNotDecrease(n){
-    return true
+    const digits = `${n}`.split("").map(v => Number(v))
+    return digits.every((digit, index) => index === 0 || digits[index - 1] <= digit )
+ 
 }
 execute()
