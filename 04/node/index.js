4a384ac53c56c68a6ac1223c0ab578f6dd6edba1
adam <adam@harpur.io>
2019-12-06 10:24:28 +0000

    chore: setup iteration

--- 04/node/index.js
+++ 04/node/index.js
@@ -3,8 +3,17 @@ const path = require('path');
 const inputPath = path.join(__dirname, 'input');
 
 async function execute(){
-    const input = fs.readFileSync(inputPath, 'utf8').split("\n")
-    console.log(input)
+    const [input] = fs.readFileSync(inputPath, 'utf8').split("\n")
+    part1(input.split("-").map(v => Number(v)))
  
+}
+
+function part1([start, end]){
+    let current = start;
+    while(current < end){
+        // validate
+        console.log(current)
+        current += 1;
+    }
 }
 execute()
