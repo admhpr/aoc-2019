071258aec8d5d124304cbd013c650acdb37d4136
adam <adam@harpur.io>
2019-12-08 15:36:02 +0000

    feat: start to calculate orbits

--- 06/node/index.js
+++ 06/node/index.js
@@ -2,19 +2,30 @@ const fs = require("fs");
 const path = require("path");
 const inputPath = path.join(__dirname, "input-test");
 
-const system = new Map();
 
 function execute() {
+  const system = new Map();
   const input = fs.readFileSync(inputPath, "utf8").split("\n");
   system.set("COM", createPlanet([null, "COM"]));
   input.forEach(orbitInfo => {
-    setValues(orbitInfo);
+    setValues(orbitInfo, system);
   });
   for (const [_, planet] of system) {
     if (planet.orbits) {
       system.get(planet.orbits).addIndirect(planet.name);
     }
   }
+  const total = calculateOrbits(system)
+}
+
+function calculateOrbits(system){
+    let total;
+    for(const [planet, info] of system){
+        const planetsTotal = 0;
+        info.children.forEach(child => {
+            console.log(planet, child)
+        })
+    }
 }
 
 function createPlanet([directOrbit, satellite]) {
@@ -28,7 +39,7 @@ function createPlanet([directOrbit, satellite]) {
   };
 }
 
-function setValues(orbitInfo) {
+function setValues(orbitInfo, system) {
   const [planet, orbitingPlanet] = orbitInfo.split(")");
   if (!system.has(orbitingPlanet)) {
     system.set(orbitingPlanet, createPlanet([planet, orbitingPlanet]));
