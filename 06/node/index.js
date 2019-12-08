51aa18070e48680f79965d8597cdfce7e13e8efc
adam <adam@harpur.io>
2019-12-08 14:57:54 +0000

    feat: build map

--- 06/node/index.js
+++ 06/node/index.js
@@ -1,35 +1,39 @@
-
 const fs = require("fs");
 const path = require("path");
 const inputPath = path.join(__dirname, "input-test");
 
 const system = new Map();
-const tree = new Map();
 
-function execute(){
-    const input = fs.readFileSync(inputPath, "utf8").split("\n");
-    system.set("COM", createPlanet([null, "COM"]))
-    input.forEach(orbitInfo => {
-        setValues(orbitInfo)
-    })
-    system.values(planet => {
-        console.log(planet)
-    })
-}
-
-function createPlanet([directOrbit, satellite]){
-    return {
-        name: satellite,
-        orbits: directOrbit,
+function execute() {
+  const input = fs.readFileSync(inputPath, "utf8").split("\n");
+  system.set("COM", createPlanet([null, "COM"]));
+  input.forEach(orbitInfo => {
+    setValues(orbitInfo);
+  });
+  for (const [_, planet] of system) {
+    if (planet.orbits) {
+      system.get(planet.orbits).addIndirect(planet.name);
     }
+  }
 }
 
-function setValues(orbitInfo){
-    const [planet, orbitingPlanet] = orbitInfo.split(')');
-    if(!system.has(orbitingPlanet)){
-        system.set(orbitingPlanet, createPlanet([planet, orbitingPlanet]))
-        return
+function createPlanet([directOrbit, satellite]) {
+  return {
+    name: satellite,
+    orbits: directOrbit,
+    children: [],
+    addIndirect(planet) {
+      this.children.push(planet);
     }
+  };
 }
 
-execute()
+function setValues(orbitInfo) {
+  const [planet, orbitingPlanet] = orbitInfo.split(")");
+  if (!system.has(orbitingPlanet)) {
+    system.set(orbitingPlanet, createPlanet([planet, orbitingPlanet]));
+    return;
+  }
+}
+
+execute();
