86ca6821cc0e6e994d8adf39ece2eb19116ae85e
adam <adam@harpur.io>
2019-12-08 14:37:18 +0000

    chore: day 6 setup

--- 06/node/index.js
+++ 06/node/index.js
@@ -3,9 +3,30 @@ const fs = require("fs");
 const path = require("path");
 const inputPath = path.join(__dirname, "input-test");
 
+const system = new Map();
+
 function execute(){
     const input = fs.readFileSync(inputPath, "utf8").split("\n");
-    console.log(input)
+    system.set("COM", createPlanet([null, "COM"]))
+    input.forEach(orbitInfo => {
+        setValues(orbitInfo)
+    })
+}
+
+function createPlanet([directOrbit, satellite]){
+    return {
+        name: satellite,
+        orbits: directOrbit,
+    }
+}
+
+function setValues(orbitInfo){
+    const [planet, orbitingPlanet] = orbitInfo.split(')');
+    if(!system.has(orbitingPlanet)){
+        system.set(orbitingPlanet, createPlanet([planet, orbitingPlanet]))
+        return
+    }
 }
 
 execute()
+console.log(system)
