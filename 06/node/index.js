15e70615d0972f67026a8891fc69449e8140cb5d
adam <adam@harpur.io>
2019-12-08 21:56:48 +0000

    feat: day 6 part 2

--- 06/node/index.js
+++ 06/node/index.js
@@ -1,52 +1,68 @@
 const fs = require("fs");
 const path = require("path");
-const inputPath = path.join(__dirname, "input-test");
+const inputPath = path.join(__dirname, "input");
 
 
 function execute() {
   const system = new Map();
+  const distancesFromCentre = new Map();
+
   const input = fs.readFileSync(inputPath, "utf8").split("\n");
-  system.set("COM", createPlanet([null, "COM"]));
+
   input.forEach(orbitInfo => {
     setValues(orbitInfo, system);
   });
-  for (const [_, planet] of system) {
-    if (planet.orbits) {
-      system.get(planet.orbits).addIndirect(planet.name);
-    }
+  for (const [satellite, parent] of system){
+      if(!distancesFromCentre.has(satellite)){
+        distancesFromCentre.set(satellite, totalOrbits(system, distancesFromCentre, satellite))
+      }
+
   }
-  const total = calculateOrbits(system)
+  console.log(`part 1: ${[...distancesFromCentre.values()].reduce((acc, cur) => acc + cur)}`)
+  // part 2
+  const myDistanceFromCentre = new Map()
+  let planet = "YOU"
+  let myDistanceFromThisPlanet = 0;
+  // my distance to COM
+  while(system.has(planet)){
+      myDistanceFromCentre.set(planet, myDistanceFromThisPlanet)
+      planet = system.get(planet)
+      myDistanceFromThisPlanet += 1;
+  }
+
+  // find the common meeting point (planet) of paths back to COM
+  let currentPlanet = "SAN"
+  let santasDistanceFromThisPlanet = 0;
+  // stops an accidental infinite loop
+  while(system.has(currentPlanet) && !myDistanceFromCentre.has(currentPlanet)){
+      currentPlanet = system.get(currentPlanet)
+      santasDistanceFromThisPlanet += 1
+  }
+
+  // distance between SAN and YOU is the distance from SAN to child plus the distance from YOU to child.
+  const distanceApart = santasDistanceFromThisPlanet + myDistanceFromCentre.get(currentPlanet)
+  console.log(`part 2: ${distanceApart - 2}`) // remove YOU and SAN
+  
 }
 
-function calculateOrbits(system){
-    var total = 0;
-    for(const [planet, info] of system){
-        const planetsTotal = info.children.length;
-        info.children.forEach(child => {
-            planetsTotal = system.get(child).children.length
-        })
-        total += planetsTotal
+function totalOrbits(system, distancesFromCentre, child){
+    if(!distancesFromCentre.has(child)){
+        if(system.get(child) === "COM"){
+            distancesFromCentre.set(child, 1)
+        }else{
+            // grabs the parents distance and adds one e.g COM --> SUN --> EARTH --> MOON
+            // if child is MOON we know the EARTH is 2 away to 1 + 2 is the MOON's distance
+            distancesFromCentre.set(child, 1 + totalOrbits(system, distancesFromCentre, system.get(child)))
+        }
     }
-    console.log(total)
-}
-
-function createPlanet([directOrbit, satellite]) {
-  return {
-    name: satellite,
-    orbits: directOrbit,
-    children: [],
-    addIndirect(planet) {
-      this.children.push(planet);
-    }
-  };
+    return distancesFromCentre.get(child)
 }
 
 function setValues(orbitInfo, system) {
-  const [planet, orbitingPlanet] = orbitInfo.split(")");
-  if (!system.has(orbitingPlanet)) {
-    system.set(orbitingPlanet, createPlanet([planet, orbitingPlanet]));
-    return;
-  }
+  const [parentPlanet, satellite] = orbitInfo.split(")");
+  // parent is the key, COM will not be in the keys
+  system.set(satellite, parentPlanet);
 }
 
 execute();
+
